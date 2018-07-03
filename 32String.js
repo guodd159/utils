2115b8ec0e5fbf7333d384a6f72c0ced89185a6f
guodd <1486175219@qq.com>
2018-07-03 15:49:39 +0800

    Add files via upload

---
+++ 32String.js
@@ -0,0 +1,10 @@
+function generateNonceString(length) {
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var maxPos = chars.length;
+    var noceStr = "";
+    for (var i = 0; i < (length || 32); i++) {
+        noceStr += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return noceStr;
+};
+console.log(generateNonceString(32))
